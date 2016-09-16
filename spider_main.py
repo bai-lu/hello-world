87a996310e25a9920c664ffb41124b2abd56a6a3
bai-lu <wardeath@live.com>
2016-09-16 19:05:36 +0800

    local craw to test

--- spider_main.py
+++ spider_main.py
@@ -2,8 +2,8 @@ import html_outputer
 import html_parser
 import html_downloader
 import url_manager
-import time
-
+from multiprocessing import Pool
+import time,os
 
 class SpiderMain(object):
     def __init__(self):
@@ -12,27 +12,26 @@ class SpiderMain(object):
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
-    def craw(self, root_url):
-        count = 1
-        self.urls.add_new_url(root_url)
-        while self.urls.has_new_url():
+    def craw(self):
+        if self.urls.has_new_url():
+            print('pid is %s' % os.getpid())
             new_url = self.urls.get_new_url()
-            print("craw %d : %s" % (count, new_url))
             html_cont = self.downloader.download(new_url)
             new_urls, new_data = self.parser.parse(new_url, html_cont)
             self.urls.add_new_urls(new_urls)
             self.outputer.collect_data(new_data)
-            if count == 500:
-                break
-            count += 1
-        # self.outputer.output_to_redis()
         self.outputer.output_to_local_file()
 
 if __name__ == '__main__':
     root_url = "http://192.168.15.132/w3school/index.html"
     obj_spider = SpiderMain()
+    obj_spider.urls.add_new_url(root_url)
     begin = time.time()
-    obj_spider.craw(root_url)
+    p=Pool(4)
+    for i in range(500):
+        p.apply_async(obj_spider.craw, args=())
+    p.close()
+    p.join()
     end = time.time()
     use = end - begin
     print('use %s s' % use)
